d31ed55358e72ef758b60e4405a173a65f5237b3
gjh33 <manhappylife@yahoo.ca>
2022-07-20 19:36:55 -0700

    First pass for scene controls

--- Assets/Scripts/DisplayMesh.cs
+++ Assets/Scripts/DisplayMesh.cs
@@ -8,9 +8,16 @@ using UnityEngine;
 /// </summary>
 public class DisplayMesh : MonoBehaviour
 {
+    public enum ControlMode { Translate, Rotate, Scale };
+    
     [SerializeField] private MeshFilter Filter;
     [SerializeField] private MeshRenderer Renderer;
+
+    public ControlMode CurrentControlMode = ControlMode.Translate;
     
+    private VisualizerControls input;
+    private bool dragging = false;
+
     /// <summary>
     /// Sets the mesh currently being displayed
     /// </summary>
@@ -36,6 +43,46 @@ public class DisplayMesh : MonoBehaviour
     public void SetTexture(Texture2D texture)
     {
         Renderer.material.mainTexture = texture;
+    }
+
+    private void OnEnable()
+    {
+        input = new VisualizerControls();
+        input.Enable();
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+    }
+
+    private void Update()
+    {
+        if (input.ModelManipulation.DragActive.WasPressedThisFrame())
+        {
+            dragging = true;
+        }
+        if (input.ModelManipulation.DragActive.WasReleasedThisFrame())
+        {
+            dragging = false;
+        }
+        if (dragging)
+        {
+            Vector2 delta = input.ModelManipulation.MoveDrag.ReadValue<Vector2>();
+            
+            if (CurrentControlMode == ControlMode.Translate)
+            {
+                transform.position += new Vector3(delta.x * 0.01f, 0, delta.y * 0.01f);
+            }
+            else if (CurrentControlMode == ControlMode.Rotate)
+            {
+                transform.Rotate(new Vector3(delta.y * 0.5f, -delta.x * 0.5f, 0), Space.World);
+            }
+            else if (CurrentControlMode == ControlMode.Scale)
+            {
+                transform.localScale += Vector3.one * delta.y * 0.01f;
+            }
+        }
     }
 
     // Attempt to resolve dependencies on create or reset
